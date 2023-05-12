6b6e22c47cccb2cfbe8c97a3fc9c750479d7f422
fatich <fatih@itsmoneo.com>
2023-05-12 20:57:35 +0300

    creating incoming offer for active user endpoint

--- src/store/actions/offers.js
+++ src/store/actions/offers.js
@@ -1,5 +1,5 @@
 import * as api from "../../api/OfferRequests.js";
-import { CREATE_OFFER, START_OFFER_LOADING, END_OFFER_LOADING } from "../../utils/constants/actionTypes.js";
+import { CREATE_OFFER, START_OFFER_LOADING, END_OFFER_LOADING, FETCH_LOGGED_USER_INCOMING_OFFERS } from "../../utils/constants/actionTypes.js";
 import {message} from "antd";
 
 
@@ -18,7 +18,7 @@ export const getLoggedUserOffers = (id) => async (dispatch) => {
     try {
         dispatch({ type: START_OFFER_LOADING });
         const { data } = await api.fetchActiveUserIncomingOffers(id);
-        dispatch({ type: FETCH_LOGGED_USER_OFFERS, payload: data });
+        dispatch({ type: FETCH_LOGGED_USER_INCOMING_OFFERS, payload: data });
         dispatch({ type: END_OFFER_LOADING });
     } catch (error) {
         message.error(error.message);
--- src/store/reducers/offers.js
+++ src/store/reducers/offers.js
@@ -1,4 +1,4 @@
-import {CREATE_OFFER, START_OFFER_LOADING, END_OFFER_LOADING} from "../../utils/constants/actionTypes.js";
+import {CREATE_OFFER, START_OFFER_LOADING, END_OFFER_LOADING, FETCH_LOGGED_USER_INCOMING_OFFERS} from "../../utils/constants/actionTypes.js";
 
 const initialState = {
     isLoading: false,
@@ -9,8 +9,9 @@ const initialState = {
 
 const offersReducer = (state = initialState, action) => {
     switch (action.type) {
-        case FETCH_LOGGED_USER_OFFERS:
+        case FETCH_LOGGED_USER_INCOMING_OFFERS:
             return {...state, activeUserIncomingOffers: action.payload.data};
+
         case CREATE_OFFER:
             return {...state, activeUserOutgoingOffers: [...state.activeUserOutgoingOffers, action.payload.data]};
         case START_OFFER_LOADING:
